f63d9db422557b91f67609e86dbf8b5475fc5663
Tim Ebringer <tim.ebringer@gmail.com>
2016-09-25 07:23:10 -0400

    Initial commit

---
+++ goauto.go
@@ -0,0 +1,9 @@
+package goauto
+
+func AddTwo(a, b int) int {
+	return a + b
+}
+
+func AddThree(a, b, c int) int {
+	return a + b
+}
---
+++ goauto_test.go
@@ -0,0 +1,21 @@
+package goauto
+
+import (
+	"testing"
+)
+
+func TestAddTwo(t *testing.T) {
+	got := AddTwo(3, 7)
+	want := 10
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestAddThree(t *testing.T) {
+	got := AddThree(3, 7, 11)
+	want := 21
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
